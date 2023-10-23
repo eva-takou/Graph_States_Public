4b04c896ea41f126a9d10fdf35feb4424477bea9
Eva Takou <etakou@vt.edu>
2023-10-23 18:40:56 -0400

    added scripts to plot quantum circuit

---
+++ Functionalities/Plot_Functions/utilities/get_Gate_Names.m
@@ -0,0 +1,17 @@
+function gate_names=get_Gate_Names
+%--------------------------------------------------------------------------
+%Created by: Eva Takou
+%
+%Last modified: Oct 23, 2023
+%--------------------------------------------------------------------------
+
+
+gate_names.X   ='X';
+gate_names.Y   ='Y';
+gate_names.Z   ='Z';
+gate_names.P   ='P';
+gate_names.H   ='H';
+gate_names.CNOT='X';
+
+
+end
